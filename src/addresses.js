4ba735ba21d27421a8320eb2269f22a7a5333a0a
Stefan <stefan.gerdin@gmail.com>
2021-06-14 18:14:51 +0200

    Adds random addresses

--- src/addresses.js
+++ src/addresses.js
@@ -1,9 +1,17 @@
 const random = require("./random");
 const { sanitizeCount } = require("./utils");
 const getLocalizedAssets = require("./assetLoader");
+const assets = require("../assets/streets.json");
+const languages = require("./languages");
 
+/**
+ * Gets a list of random street names.
+ * @param {string} language Language in which the names are common.
+ * @param {number} count Number of streets generated.
+ * @returns An array of street names.
+ */
 const streets = (language, count) => {
-  const streets = getLocalizedAssets(language);
+  const streets = getLocalizedAssets(language, assets);
 
   count = sanitizeCount(count);
 
@@ -16,4 +24,35 @@ const streets = (language, count) => {
   return randomStreets;
 };
 
+/**
+ * Gets a list of random address, i.e. street name and number.
+ * @param {string} language Language in which the names are common.
+ * @param {number} count Number of addresses generated.
+ * @returns An array of address names.
+ */
+const addresses = (language, count) => {
+  const randomStreets = streets(language, count);
+
+  for (let i = 0; i < randomStreets.length; i++) {
+    let street = randomStreets[i];
+    switch (language) {
+      case languages.english:
+        // todo: remove names with number
+        // todo: add compass points
+        street = (random.randomInt(10000) + 1).toString() + " " + street;
+        break;
+      case languages.swedish:
+        street = street + " " + random.randomInt(98) + 1;
+        break;
+      default:
+        throw "Unknown language: " + language;
+    }
+
+    randomStreets[i] = street;
+  }
+
+  return randomStreets;
+};
+
 exports.streets = streets;
+exports.addresses = addresses;
--- src/addresses.spec.js
+++ src/addresses.spec.js
@@ -2,9 +2,17 @@ const addresses = require("./addresses");
 const english = require("./languages").english;
 
 describe("streets()", () => {
-  it("Can generate correct number streets", () => {
+  test("Generate correct number of streets", () => {
     const count = 1000;
     let randomAddresses = addresses.streets(english, count);
+    expect(randomAddresses.length).toEqual(count);
+  });
+});
+
+describe("addresses()", () => {
+  test("Generate correct number of addresses", () => {
+    const count = 1000;
+    let randomAddresses = addresses.addresses(english, count);
     expect(randomAddresses.length).toEqual(count);
   });
 });
