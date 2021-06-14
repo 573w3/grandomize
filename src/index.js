4957e8c5a0d0d408e00b63b50e87477882eff62a
Stefan <stefan.gerdin@gmail.com>
2021-06-14 18:15:46 +0200

    Adds address exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 const names = require("./names");
+const addresses = require("./addresses");
 const languages = require("./languages");
 
 const grandomize = {
   firstNames: names.firstNames,
   lastNames: names.lastNames,
+  streets: addresses.streets,
+  addresses: addresses.addresses,
   languages: languages,
 };
 
